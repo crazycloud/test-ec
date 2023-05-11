a1c4a9849df13cee82164ed2764bd4486b11a362
crazycloud <stitch@Sourabhs-MacBook-Pro.local>
2023-05-11 17:26:14 -0400

    draft

---
+++ serve.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def stress_cpu():
+    # create a subprocess for running "stress_cpu.py"
+    subprocess.Popen(['python', 'stress_cpu.py'])
+    return 'CPU stress test initiated'
+
+
+@app.route('/', methods=['GET'])
+def get_private_ip():
+    # return the private IP address of the EC2 instance
+    private_ip = socket.gethostbyname(socket.gethostname())
+    return f'The private IP address is {private_ip}'
+
+
+if __name__ == '__main__':
+    app.run()
